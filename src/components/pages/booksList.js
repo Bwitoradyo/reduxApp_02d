1323fff6495c4c5e6651844177e44c4a0753e3af
bwitoradyo <bambang.witoradyo@gmail.com>
2017-10-29 19:35:35 -0400

    first commit

---
+++ src/components/pages/booksList.js
@@ -0,0 +1,61 @@
+"use strict";
+
+import React from "react";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import {Grid, Row, Col, Button} from "react-bootstrap";
+import {getBooks} from "../../actions/booksActions";
+
+import BookItem from "./bookItem";
+import BooksForm from "./booksForm";
+import Cart from "./cart";
+
+class BooksList extends React.Component{
+  
+  componentDidMount(){
+    this.props.getBooks()
+  }	
+	
+  render(){
+    const booksList = this.props.books.map(
+      (booksArr) => {
+         return(
+	   <Col xs={12} sm={6} md={4} key={booksArr._id}>
+	     <BookItem
+	       _id={booksArr._id}
+	       title={booksArr.title}
+	       description={booksArr.description}
+	       images={booksArr.images}
+	       price={booksArr.price}
+	     />
+	   </Col>
+	 )
+      }		    
+    )
+    return(
+      <Grid>
+        <Row>
+	  <Cart />
+	</Row>
+        <Row style={{marginTop:"15px"}}>
+	  {booksList} 
+	</Row>
+      </Grid>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return{
+    books: state.books.books
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    {
+      getBooks: getBooks
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
